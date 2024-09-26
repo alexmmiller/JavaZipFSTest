c2d3c02a23a6b06830a58f9f71982f6b8dfaa3d2
alex <alex@debian.home>
2024-09-26 14:16:11 -0500

    Time compression

--- Test.java
+++ Test.java
@@ -17,7 +17,7 @@ public class Test {
         Stream<Path> file_stream = Files.list(Paths.get(rand_files_dir_name));
         int file_count = file_stream.collect(Collectors.toList()).size();
 
-        out.print(file_count);
+        ArrayList<Integer> runtimes = new ArrayList<Integer>();
 
         URI uri = URI.create("jar:file:" + zip_path);
         boolean deleted = Files.deleteIfExists(Paths.get(zip_path));
@@ -28,12 +28,29 @@ public class Test {
             Files.createDirectory(rand_dir);
 
             for(int i = 1; i <= file_count; i++) {
+                long startTime = System.nanoTime();
+
                 String rand_file = rand_files_dir_name + "/" + Integer.toString(i) + "_rand.txt";
 
                 Path externalTxtFile = Paths.get(rand_file);
                 Path pathInZip = zipfs.getPath(rand_file);
                 Files.copy(externalTxtFile, pathInZip, StandardCopyOption.REPLACE_EXISTING);
+
+                long endTime = System.nanoTime();
+
+                int runtime = (int)(endTime - startTime)/1000000;
+
+                out.println(Integer.toString(i) + " -- " + Integer.toString(runtime) + "ms");
+
+                runtimes.add(runtime);
             }
         }
+
+        double runtimeSum = 0;
+        for(int i = 0; i < runtimes.size(); i++) {
+            runtimeSum += runtimes.get(i);
+        }
+
+        out.print("Avg: " + Double.toString(runtimeSum / runtimes.size()));
     }
 }
